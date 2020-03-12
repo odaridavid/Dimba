bb2f868464e1ced6566991c8851864bc7213be6f
Davidodari <davidkibzodari@gmail.com>
2020-03-12 23:29:38 +0300

    add license info

--- app/src/main/java/com/github/odaridavid/dimba/DimbaApplication.kt
+++ app/src/main/java/com/github/odaridavid/dimba/DimbaApplication.kt
@@ -3,11 +3,19 @@ package com.github.odaridavid.dimba
 import android.app.Application
 
 /**
- * Created By David Odari
- * On 12/03/20
+ *
+ * Copyright 2020 David Odari
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
  **/
-class DimbaApplication:Application() {
+class DimbaApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
