2dee64553779bcd86288f6a46e6f5a73faa74ba3
Davidodari <davidkibzodari@gmail.com>
2020-03-12 22:50:47 +0300

    Initial commit

---
+++ app/src/main/java/com/github/odaridavid/dimba/DimbaApplication.kt
@@ -0,0 +1,15 @@
+package com.github.odaridavid.dimba
+
+import android.app.Application
+
+/**
+ * Created By David Odari
+ * On 12/03/20
+ *
+ **/
+class DimbaApplication:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/github/odaridavid/dimba/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.github.odaridavid.dimba
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
