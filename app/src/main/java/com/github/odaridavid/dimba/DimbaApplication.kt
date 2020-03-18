ab1760099fbcf5e39a48e97d5c5ea65b5739ff43
Davidodari <davidkibzodari@gmail.com>
2020-03-19 00:08:24 +0300

    chore #30: setup networking layer

--- app/src/main/java/com/github/odaridavid/dimba/DimbaApplication.kt
+++ app/src/main/java/com/github/odaridavid/dimba/DimbaApplication.kt
@@ -1,6 +1,11 @@
 package com.github.odaridavid.dimba
 
 import android.app.Application
+import com.github.odaridavid.dimba.di.network
+import com.github.odaridavid.dimba.di.presentation
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
 import timber.log.Timber
 
 /**
@@ -22,5 +27,11 @@ class DimbaApplication : Application() {
         super.onCreate()
         if (BuildConfig.DEBUG)
             Timber.plant(Timber.DebugTree())
+
+        startKoin {
+            androidLogger()
+            androidContext(this@DimbaApplication)
+            modules(listOf(network))
+        }
     }
 }
